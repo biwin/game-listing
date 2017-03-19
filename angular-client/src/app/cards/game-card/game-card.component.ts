980642c71648165c9259cd0c4db62e71ef536e20
biwin <biwin007@gmail.com>
2017-03-19 13:50:20 +0530

    adds input game

--- angular-client/src/app/cards/game-card/game-card.component.ts
+++ angular-client/src/app/cards/game-card/game-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Game} from "../../interfaces/game";
 
 @Component({
   selector: 'app-game-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-card.component.sass']
 })
 export class GameCardComponent implements OnInit {
+
+  @Input() game: Game;
 
   constructor() { }
 
