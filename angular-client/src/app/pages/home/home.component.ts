5b067abe1c7a280e8ecdb9bb65f8bbee4f74198a
biwin <biwin007@gmail.com>
2017-03-19 13:48:19 +0530

    gets games list on Init

--- angular-client/src/app/pages/home/home.component.ts
+++ angular-client/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {GameService} from "../../services/game.service";
+import {Game} from "../../interfaces/game";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  private games: Game[];
 
-  constructor() { }
+  constructor(private _game: GameService) { }
 
   ngOnInit() {
+    this._game.getGameList().subscribe(res=>{
+      this.games = res;
+    })
+
   }
 
 }
