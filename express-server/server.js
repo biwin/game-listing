cc4a5352979df7bc4581cbbe39d26a1639b6dc97
biwin <biwin007@gmail.com>
2017-03-19 11:30:39 +0530

    Test Server

--- express-server/server.js
+++ express-server/server.js
@@ -1,3 +1,15 @@
 /**
  * Created by biwin on 19/3/17.
  */
+var express = require('express');
+var app = express();
+
+
+app.get('/', function (req, res) {
+    res.send('API Server Works!');
+});
+
+
+app.listen(3000, function () {
+    console.log('Express Server running on PORT 3000')
+});
