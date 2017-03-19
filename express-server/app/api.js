9716a209e3cc032000fa14fcfd9e118a0dc4d04b
biwin <biwin007@gmail.com>
2017-03-19 19:18:07 +0530

    hooks up authentication.

--- express-server/app/api.js
+++ express-server/app/api.js
@@ -6,9 +6,11 @@ var express = require('express');
 var router = express.Router();
 var csv = require('csvtojson');
 var request = require('request');
+const auth = require('../app/auth');
+const passport = require('passport');
 
 // CONNECT TO MONGO
-const dbHost = 'mongodb://database/games-docked';
+const dbHost = 'mongodb://localhost:27017/games-docked';
 mongoose.connect(dbHost);
 
 
@@ -30,7 +32,7 @@ router.get('/', function (req, res) {
     res.send('API Server Works!');
 });
 
-router.get('/games', function (req, res) {
+router.get('/games', passport.authenticate('basic', { session: false }),  function (req, res) {
     var searchTerm = req.query.query;
     if (searchTerm){
         Game.find({title: { $regex: '(' + searchTerm + ')', '$options': 'i' }}, function(err, games){
@@ -47,13 +49,18 @@ router.get('/games', function (req, res) {
     }
 });
 
-router.get('/games/:id', function(req, res){
+router.get('/games/:id', passport.authenticate('basic', { session: false }), function(req, res){
     //noinspection JSUnresolvedFunction
     Game.findById(req.params.id, function(err, game){
         if (err) res.status(500).send(err);
         res.status(200).json(game);
     })
 });
+
+router.post('/users', function(req, res) {
+    auth.createNewUser(req, res);
+});
+
 
 // HELPER FUNCTIONS
 function InitDatabase() {
